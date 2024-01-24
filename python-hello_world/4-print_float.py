fc5f2d01f80ac50ac4c7e1bb344b2eda00d00d2c
fatimasgit <fatimamedova@inbox.ru>
2024-01-24 11:53:07 -0800

    Hello world - Python

---
+++ python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 3.14159
+cut = "Float: {:.2f}\n".format(number)
+print(cut) 
---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+slice = str[:9]
+print(str*3 + "\n" + slice)
