d95f3482cb7fd32b6a242c2b0787eb2d37b4c500
fatimasgit <fatimamedova@inbox.ru>
2024-01-25 11:23:13 -0800

    changes

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 number = 98
-print(f"{98} Battery street")
+str = "Battery street"
+print(f"{98} {str}")
--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-cut = "Float: {:.2f}".format(number)
+cut = f"Float: {number:.2f}"
 print(cut)
